31859eca2f9c121bfcbe74015e525668ffdba2ff
natanfudge <natan.lifsiz@gmail.com>
2019-10-20 13:31:51 +0300

    commit before rip

---
+++ cmake-build-debug/CMakeFiles/SkiaTest.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/natan/Desktop/SkiaTest/src/main.cpp" "C:/Users/natan/Desktop/SkiaTest/cmake-build-debug/CMakeFiles/SkiaTest.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "NOMINMAX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs/skia"
+  "../libs/skia/include/core"
+  "../libs/skia/include/gpu"
+  "../libs/skia/include/config"
+  "../libs/skia/include/utils"
+  "../"
+  "../libs/glew/include/GL"
+  "../libs/sdl/include"
+  "../libs/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SkiaTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SkiaTest.dir/src/main.cpp.obj"
+  "SkiaTest.exe"
+  "SkiaTest.exe.manifest"
+  "SkiaTest.lib"
+  "SkiaTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SkiaTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
